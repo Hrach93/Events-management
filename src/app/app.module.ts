88332d4b363643b8f68dec2309b249d969f74adc
Hrach93 <hrach600@mail.ru>
2020-01-07 16:28:01 +0400

    events-page-v0 and admin-page-v0

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { EventPageComponent } from './event-page/event-page.component';
+import {EventComponent} from './shared/components/event/event.component';
+import {SharedModule} from './shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    EventPageComponent,
+    EventComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
